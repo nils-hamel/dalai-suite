87fbbb4bf5ab2e183d8710a7188230c361915052
nils-hamel <nils.hamel@bluewin.ch>
2016-04-22 13:32:22 +0200

    Initial commit

---
+++ src/dalai-ply-universal/src/dalai-ply-universal.hpp
@@ -0,0 +1,95 @@
+/*
+ *  dalai-suite - geodetic system
+ *
+ *      Nils Hamel - nils.hamel@bluewin.ch
+ *      Copyright (c) 2016 EPFL CDH DHLAB
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+    /*! \file   dalai-ply-universal.h
+     *  \author Nils Hamel <n.hamel@bluewin.ch>
+     *
+     *  Polygon File Format to Universal format converter
+     */
+
+    /*! \mainpage dalai-suite
+     *
+     *  \section dalai-suite
+     * 
+     *  ...
+     *
+     *  \section Copyright and License
+     *
+     *  ...
+     */
+
+/*
+    header - inclusion guard
+ */
+
+    # ifndef __DL_PLY_UNIVERSAL__
+    # define __DL_PLY_UNIVERSAL__
+
+/*
+    header - C/C++ compatibility
+ */
+
+    # ifdef __cplusplus
+    extern "C" {
+    # endif
+
+/*
+    header - includes
+ */
+
+/*
+    header - preprocessor definitions
+ */
+
+/*
+    header - preprocessor macros
+ */
+
+/*
+    header - type definition
+ */
+
+/*
+    header - structures
+ */
+
+/*
+    header - function prototypes
+ */
+
+    /*! \brief main function
+     */
+
+    int main( int argc, char ** argv );
+
+/*
+    header - C/C++ compatibility
+ */
+
+    # ifdef __cplusplus
+    }
+    # endif
+
+/*
+    header - inclusion guard
+ */
+
+    # endif
+
