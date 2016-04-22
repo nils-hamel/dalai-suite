c3b4779d80f51b6c4c0929873216720973f4018c
nils-hamel <nils.hamel@bluewin.ch>
2016-04-22 15:04:19 +0200

    Doxygen documentation

--- src/dalai-ply-universal/src/dalai-ply-universal.hpp
+++ src/dalai-ply-universal/src/dalai-ply-universal.hpp
@@ -76,6 +76,16 @@
  */
 
     /*! \brief main function
+     *
+     *  The main function reads the paths of the input and output files and
+     *  the provided time. When no time is specified, current UNIX timestamp is
+     *  considered. The function then reads the content of the input file and
+     *  convert the information into the universal standard before to export it
+     *  in the output file. From this point of view, the main function simply
+     *  implements a format converter.
+     *
+     *  \param argc Standard parameter
+     *  \param argv Standard parameter
      */
 
     int main( int argc, char ** argv );
