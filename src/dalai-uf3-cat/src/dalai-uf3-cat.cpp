5bfe8aa6adc7d7d5dcbfb629d68e2d23e91bfc31
nils-hamel <nils.hamel@bluewin.ch>
2017-07-18 14:48:53 +0200

    Initial version of uf3-cat

---
+++ src/dalai-uf3-cat/src/dalai-uf3-cat.cpp
@@ -0,0 +1,84 @@
+/*
+ *  dalai-suite - uf3-ply
+ *
+ *      Nils Hamel - nils.hamel@bluewin.ch
+ *      Copyright (c) 2016-2017 EPFL CDH DHLAB
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+    # include "dalai-uf3-cat.hpp"
+
+/*
+    source - main methods
+ */
+
+    int main( int argc, char ** argv ) {
+
+        /* stream record pointers */
+        double * dl_pose( nullptr );
+
+        /* stream record pointers */
+        char * dl_data( nullptr );
+
+        /* stream buffer variables */
+        char dl_buffer[LC_UF3_RECLEN];
+
+        /* stream variables */
+        std::ifstream dl_stream( lc_read_string( argc, argv, "--uf3", "-i" ), std::ios::in | std::ios::binary );
+
+        /* check stream */
+        if ( dl_stream.is_open() != true ) {
+
+            /* display message */
+            std::cerr << "dalai-suite : error : unable to access input stream" << std::endl;
+
+            /* send message */
+            return( EXIT_FAILURE );
+
+        }
+
+        /* create record pointers */
+        dl_pose = ( double * ) dl_buffer;
+
+        /* create record pointers */
+        dl_data = dl_buffer + sizeof( double ) * 3;
+
+        /* stream reading loop */
+        do {
+
+            /* read stream chunk */
+            dl_stream.read( dl_buffer, LC_UF3_RECLEN );
+
+            /* check reading */
+            if ( dl_stream.gcount() == LC_UF3_RECLEN ) {
+
+                /* display position */
+                printf( "%e %e %e ", dl_pose[0], dl_pose[1], dl_pose[2] );
+
+                /* display data */
+                printf( "%02x %02x %02x\n", dl_data[0], dl_data[1], dl_data[2] );
+
+            }
+
+        } while ( dl_stream.gcount() > 0 );
+
+        /* delete stream */
+        dl_stream.close();
+
+        /* send message */
+        return( EXIT_SUCCESS );
+
+    }
+
---
+++ src/dalai-uf3-cat/src/dalai-uf3-cat.hpp
@@ -0,0 +1,108 @@
+/*
+ *  dalai-suite - uf3-ply
+ *
+ *      Nils Hamel - nils.hamel@bluewin.ch
+ *      Copyright (c) 2016-2017 EPFL CDH DHLAB
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+    /*! \file   dalai-uf3-ply.hpp
+     *  \author Nils Hamel <nils.hamel@bluewin.ch>
+     *
+     *  dalai-suite - uf3-ply
+     */
+
+    /*! \mainpage dalai-suite
+     *
+     *  \section _1 dalai-suite
+     *
+     *  The _dalai-suite_ is dedicated to the gathering and processing of
+     *  geographical 3-dimensional information. It allows to considers the most
+     *  common file formats and to convert them in a standardised and simple
+     *  format.
+     *
+     *  This standardised format allows to use the suite tools for color
+     *  edition, model cleaning and model hashing. In addition, the standard
+     *  format is also expected by the _eratosthene-suite_ implementing the EPFL
+     *  CDH DHLAB indexation server and its geographical 3-dimensional data
+     *  injection tools.
+     *
+     *  \section _2 Copyright and License
+     *
+     *  **dalai-suite** - Nils Hamel <br >
+     *  Copyright (c) 2016-2017 EPFL CDH DHLAB
+     *
+     *  This program is licensed under the terms of the GNU GPLv3.
+     */
+
+/*
+    header - inclusion guard
+ */
+
+    # ifndef __DL_UF3_CAT__
+    # define __DL_UF3_CAT__
+
+/*
+    header - internal includes
+ */
+
+/*
+    header - external includes
+ */
+
+    # include <iostream>
+    # include <fstream>
+    # include <common-include.hpp>
+
+/*
+    header - preprocessor definitions
+ */
+
+/*
+    header - preprocessor macros
+ */
+
+/*
+    header - type definition
+ */
+
+/*
+    header - structures
+ */
+
+/*
+    header - function prototypes
+ */
+
+    /*! \brief main methods
+     *
+     *
+     *      ./dalai-uf3-cat --uf3/-i [uf3 input file]
+     *
+     *
+     *  \param  argc Standard parameter
+     *  \param  argv Standard parameter
+     *
+     *  \return Returns standard exit code
+     */
+
+    int main( int argc, char ** argv );
+
+/*
+    header - inclusion guard
+ */
+
+    # endif
+
