693b261e8127a58cd9cbe559de6ffff3ed4de12a
nils-hamel <nils.hamel@bluewin.ch>
2019-03-14 13:41:11 +0100

    Adding doxygen documentation in interlis

--- src/dalai-interlis/src/dalai-interlis.hpp
+++ src/dalai-interlis/src/dalai-interlis.hpp
@@ -90,17 +90,57 @@
     header - function prototypes
  */
 
-    /* *** */
+    /*! \brief buffer methods
+     *
+     *  This function is used to initialise the content of the provided uv3
+     *  record buffer. It simply sets the position to zeros and assign the
+     *  provided color. In addition, it also sets the primitive type to two
+     *  expecting only lines from INTERLIS format.
+     *
+     *  \param dl_buffer uv3 record buffer
+     *  \param dl_red    uv3 record red color
+     *  \param dl_green  uv3 record green color
+     *  \param dl_blue   uv3 record blue color
+     */
 
     void dl_interlis_init( le_byte_t * dl_buffer, le_byte_t const dl_red, le_byte_t const dl_green, le_byte_t const dl_blue );
 
-    /* *** */
+    /*! \brief buffer methods
+     *
+     *  This function is used to assign a planimetric position to the provided
+     *  uv3 record buffer before to export it in the provided stream. The
+     *  provided stream has to be an already open stream in binary mode.
+     *
+     *  \param dl_buffer uv3 record buffer
+     *  \param dl_stream Exportation stream
+     *  \param dl_x      Planimetric x position
+     *  \param dl_y      Planimetric y position
+     */
 
     void dl_interlis_export( le_byte_t * const dl_buffer, std::ofstream & dl_stream, le_real_t const dl_x, le_real_t const dl_y );
 
     /*! \brief main methods
      *
+     *  The main function opens and reads the provided INTERLIS file and extract
+     *  the targeted data before to convert and export them in uv3 format :
      *
+     *      ./dalai-interlis --interlis/-i [input file]
+     *                       --uv3/-o [output file]
+     *                       --red/-r [uv3 record color]
+     *                       --green/-g [uv3 record color]
+     *                       --blue/-b [uv3 record color]
+     *                       --topic/-t [interlis target topic]
+     *                       --table/-a [interlis target table]
+     *
+     *  The main function starts by locating the target topic in the provided
+     *  interlis data file. In the found topic, the target table is searched.
+     *  The content of the table is then read object by object to extract the
+     *  geomtry.
+     *
+     *  The geometry is converted into uv3 lines primitive before to be exported
+     *  in the output uv3 file. The provided color is used to assign the uv3
+     *  primitive color.
+
      *  \param  argc Standard parameter
      *  \param  argv Standard parameter
      *
