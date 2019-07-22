68464fd5462d19667af4b7f746350760ee519271
nils-hamel <nils.hamel@bluewin.ch>
2019-07-22 14:42:23 +0200

    Adding geoid height conversion software - initial tested version

---
+++ src/dalai-geoid/src/dalai-geoid.cpp
@@ -0,0 +1,170 @@
+/*
+ *  dalai-suite - geoid
+ *
+ *      Nils Hamel - nils.hamel@bluewin.ch
+ *      Copyright (c) 2016-2019 DHLAB, EPFL
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+    # include "dalai-geoid.hpp"
+
+/*
+    source - conversion methods
+ */
+
+    le_void_t dl_geoid_height( le_char_t const * const dl_input, le_char_t const * const dl_output, GeographicLib::Geoid & dl_geoid, le_real_t const dl_conversion ) {
+
+        /* data buffer variable */
+        le_byte_t dl_buffer[LE_ARRAY_DATA];
+
+        /* data pointer variable */
+        le_real_t * dl_pose( nullptr );
+
+        /* readind variable */
+        le_size_t dl_read( 1 );
+
+        /* stream variable */
+        std::ifstream dl_istream( ( char * ) dl_input, std::ios::binary );
+
+        /* stream variable */
+        std::ofstream dl_ostream( ( char * ) dl_output, std::ios::binary );
+
+        /* check stream */
+        if ( dl_istream.is_open() == false ) {
+
+            /* send message */
+            throw( LC_ERROR_IO_READ );
+
+        }
+
+        /* check stream */
+        if ( dl_ostream.is_open() == false ) {
+
+            /* send message */
+            throw( LC_ERROR_IO_WRITE );
+
+        }
+
+        /* parsing input stream */
+        while ( dl_read > 0 ) {
+
+            /* read data chunk */
+            dl_istream.read( ( char * ) dl_buffer, LE_ARRAY_DATA );
+
+            /* check read chunk */
+            if ( ( dl_read = dl_istream.gcount() ) > 0 ) {
+
+                /* parsing chunk vertex */
+                for ( le_size_t dl_parse( 0 ); dl_parse < dl_read; dl_parse += LE_ARRAY_DATA ) {
+
+                    /* create data pointer */
+                    dl_pose = ( le_real_t * ) ( dl_buffer + dl_parse );
+
+                    /* vertex conversion */
+                    dl_pose[2] = dl_pose[2] + dl_conversion * dl_geoid( dl_pose[1] * LE_R2D, dl_pose[0] * LE_R2D );
+
+                }
+
+                /* export processed chunk */
+                dl_ostream.write( ( char * ) dl_buffer, dl_read );
+
+            }
+
+        }
+
+        /* delete stream */
+        dl_ostream.close();
+
+        /* delete stream */
+        dl_istream.close();
+
+    }
+
+/*
+    source - main methods
+ */
+
+    int main( int argc, char ** argv ) {
+
+        /* geoid name variable */
+        le_char_t * dl_name = ( le_char_t * ) lc_read_string( argc, argv, "--name", "-n" );
+
+        /* geoid path variable */
+        le_char_t * dl_path = ( le_char_t * ) lc_read_string( argc, argv, "--path", "-p" );
+
+        /* i/o path variable */
+        le_char_t * dl_input = ( le_char_t * ) lc_read_string( argc, argv, "--input", "-i" );
+
+        /* i/o path variable */
+        le_char_t * dl_output = ( le_char_t * ) lc_read_string( argc, argv, "--output", "-o" );
+
+        /* conversion variable */
+        le_real_t dl_conversion( 0.0 );
+
+    /* error management */
+    try {
+
+        /* geolib geoid variable */
+        GeographicLib::Geoid dl_geoid( std::string( ( char * ) dl_name ), std::string( ( char * ) dl_path ), true );
+
+        /* conversion detection */
+        if ( lc_read_flag( argc, argv, "--to-msl", "-m" ) == true ) {
+
+            /* update conversion */
+            dl_conversion = -1.0;
+
+        } else if ( lc_read_flag( argc, argv, "--to-ell", "-e" ) == true ) {
+
+            /* update conversion */
+            dl_conversion = +1.0;
+
+        }
+
+        /* check execution mode */
+        if ( lc_file_detect( ( char * ) dl_input ) == LC_DIRECTORY ) {
+
+            /* check consistency */
+            if ( lc_file_detect( ( char * ) dl_output ) != LC_DIRECTORY ) {
+
+                /* send message */
+                throw( LC_ERROR_IO_WRITE );
+
+            }
+
+            /* *** */
+
+        } else {
+
+            /* process file */
+            dl_geoid_height( dl_input, dl_output, dl_geoid, dl_conversion );
+
+        }
+
+    /* error management */
+    } catch ( int dl_code ) {
+
+        /* error management */
+        lc_error( dl_code );
+
+        /* send message */
+        return( EXIT_FAILURE );
+
+    }
+
+        /* send message */
+        return( EXIT_SUCCESS );
+
+    }
+
---
+++ src/dalai-geoid/src/dalai-geoid.hpp
@@ -0,0 +1,117 @@
+/*
+ *  dalai-suite - geoid
+ *
+ *      Nils Hamel - nils.hamel@bluewin.ch
+ *      Copyright (c) 2016-2019 DHLAB, EPFL
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+    /*! \file   dalai-cat.hpp
+     *  \author Nils Hamel <nils.hamel@bluewin.ch>
+     *
+     *  dalai-suite - geoid
+     */
+
+    /*! \mainpage dalai-suite
+     *
+     *  \section overview Overview
+     *
+     *  The _dalai-suite_ is part of the Eratosthene Project and is dedicated to
+     *  3D models gathering, processing and format conversion. It allows to
+     *  consider the most common formats and to convert them into the project
+     *  common format. The _dalai-suite_ is mainly used for data gathering and
+     *  processing, allowing to make them compatible with remote Eratosthene
+     *  servers injection format.
+     *
+     *  In addition, the project common format allows to apply the _dalai-suite_
+     *  solutions for 3D models processing. The suite offers solution for
+     *  massive 3D models cleaning, hashing and assisted geographical
+     *  registration. The suite also provides solution for 3D models display and
+     *  analysis.
+     *
+     *  \section copyright Copyright and License
+     *
+     *  **dalai-suite** - Nils Hamel <br >
+     *  Copyright (c) 2016-2019 DHLAB, EPFL
+     *
+     *  This program is licensed under the terms of the GNU GPLv3.
+     */
+
+/*
+    header - inclusion guard
+ */
+
+    # ifndef __DL_GEOID__
+    # define __DL_GEOID__
+
+/*
+    header - internal includes
+ */
+
+/*
+    header - external includes
+ */
+
+    # include <iostream>
+    # include <fstream>
+    # include <common-include.hpp>
+    # include <eratosthene-include.h>
+    # include <GeographicLib/Geoid.hpp>
+
+/*
+    header - preprocessor definitions
+ */
+
+/*
+    header - preprocessor macros
+ */
+
+    /* define data chunk */
+    # define DL_CHUNK ( le_size_l( 37449 ) * LE_ARRAY_DATA )
+
+/*
+    header - type definition
+ */
+
+/*
+    header - structures
+ */
+
+/*
+    header - function prototypes
+ */
+
+    /* *** */
+
+    le_void_t dl_geoid_height( le_char_t const * const dl_input, le_char_t const * const dl_output, GeographicLib::Geoid & dl_geoid, le_real_t const dl_conversion );
+
+    /*! \brief main methods
+     *
+     *      ./dalai-geoid -- []
+     *
+     *  \param  argc Standard parameter
+     *  \param  argv Standard parameter
+     *
+     *  \return Returns standard exit code
+     */
+
+    int main( int argc, char ** argv );
+
+/*
+    header - inclusion guard
+ */
+
+    # endif
+
